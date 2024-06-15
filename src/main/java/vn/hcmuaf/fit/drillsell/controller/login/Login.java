b7fcf3b9568634fdb725661e1e97d963ae6cc3c3
PhamThiNgocHoa <127472698+PhamThiNgocHoa@users.noreply.github.com>
2024-06-16 02:15:57 +0700

    Hoàn thành chức năng ghi lại lịch sử đăng nhập log, giới hạn số lần đăng nhập và khóa tài khoản khi đăng nhập vào tài khoản với số lần sai quá mức cho phép và show ra được giao diện log

--- src/main/java/vn/hcmuaf/fit/drillsell/controller/login/Login.java
+++ src/main/java/vn/hcmuaf/fit/drillsell/controller/login/Login.java
@@ -11,6 +11,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.time.Instant;
+import java.util.Optional;
 
 public class Login {
     public static void login(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
@@ -24,18 +26,28 @@ public class Login {
         System.out.println(username + password);
 
         if (validInput(username, password)) {
+            User user = UsersDAO.getInstance().getUserByUsername(username);
+
+            if (user != null) {
+                Optional<Instant> lockedUntil = UsersDAO.getInstance().getLockedUntil(user.getId());
+                if (lockedUntil.isPresent() && lockedUntil.get().isAfter(Instant.now())) {
+                    Notify.getInstance().sendNotify(session, request, response, "Tài khoản của bạn đã bị khóa. Vui lòng đăng nhập lại sau 10 phút.", "login.jsp");
+                    return;
+                }
+            }
+
             // Mã hóa mật khẩu người dùng nhập sau đó so sánh với mật khẩu đã mã hóa trong database
             User auth = UsersDAO.getInstance().getUser(username, UsersDAO.getInstance().hashPassword(password));
-//            Tìm thấy thông tin người dùng
-            if (auth != null) {//Nếu tìm thấy thông tin người dùng thì sẽ set userId trong log là id của người dùng
 
+            // Tìm thấy thông tin người dùng
+            if (auth != null) {
+                // Nếu tìm thấy thông tin người dùng thì sẽ set userId trong log là id của người dùng
                 log.setUserId(auth.getId());
                 session.setAttribute("auth", auth); // Gửi thông tin tài khoản để frontend xử lý
                 LogDAO.insertLoginTrue(log); // Ghi nhật ký đăng nhập thành công
 
                 response.sendRedirect("home.jsp");
             } else {
-                User user = UsersDAO.getInstance().getUserByUsername(username);
                 // Ghi nhật ký đăng nhập thất bại trước khi chuyển hướng
                 if (user != null) {
                     // Lấy userId của người dùng từ dữ liệu
